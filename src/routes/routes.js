a72f85e0f548b6dc87e3d35738889c986a085c9f
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2023-10-23 13:43:11 -0300

    Login FullStack

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Login" });
 });
 
+router.get("/signup", (req, res) => {
+  res.render("signup", { title: "Cadastrar-se" });
+});
+
 module.exports = router;
